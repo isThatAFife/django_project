7083208c54e96490f8af088ef07d09bbcc90994f
D <dylankfilm@gmail.com>
2024-11-18 17:00:49 +0900

    edited wsgi

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xvg^sn#qq0k3)sn523_st!=g3(+ri_6^%fjythg7#i$pqpvqar
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
